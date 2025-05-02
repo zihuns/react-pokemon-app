0b9e922ad75d409ad2f9ab9c7c916603b7060294
JI HUN SEO <jhseo0313@naver.com>
2025-05-02 21:04:30 +0900

    init: vite, tainwindcss, pokemon api, pokemon card, lazy loading, debounce

---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import LazyImage from "./LazyImage";
+
+const PokeCard = ({ url, name }) => {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    fetchPokeDetailData();
+  }, []);
+
+  async function fetchPokeDetailData() {
+    try {
+      const response = await axios.get(url);
+      const pokemonData = formatPokemonData(response.data);
+      console.log(pokemonData);
+      setPokemon(pokemonData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function formatPokemonData(params) {
+    const { id, name, types } = params;
+    const PokeData = {
+      id,
+      name,
+      types: types[0].type.name,
+    };
+    return PokeData;
+  }
+
+  const bg = `bg-${pokemon?.types}`;
+  const border = `border-${pokemon?.types}`;
+  const text = `text-${pokemon?.types}`;
+
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`;
+
+  return (
+    <>
+      {pokemon && (
+        <a
+          href={`/pokemon/${name}`}
+          className={`box-border rounded-lg ${border} w-[8.5rem] h-[8.5rem] z-0 bg-slate-800 justify-between items-center`}
+        >
+          <div
+            className={`${text} h-[1.5rem] text-xs w-full pt-1 px-2 text-right rounded-t-lg`}
+          >
+            #{pokemon.id.toString().padStart(3, "00")}
+          </div>
+          <div className={`w-full f-6 flex items-center justify-center`}>
+            <div
+              className={`box-border relative flex w-full h-[5.5rem] basis justify-center items-center`}
+            >
+              <LazyImage url={img} alt={name} />
+            </div>
+          </div>
+          <div
+            className={`${bg} text-xs text-zinc-100 h-[1.5rem] rounded-b-lg uppercase font-medium pt-1`}
+          >
+            {pokemon.name}
+          </div>
+        </a>
+      )}
+    </>
+  );
+};
+
+export default PokeCard;
